13f20d2eb2102ea8cece29c674b14b2412078ca8
Xie Chenhong <xiech@garena.com>
2019-08-10 20:59:50 +0800

    UPD: Use BehaviorSubject directly

--- src/index.ts
+++ src/index.ts
@@ -1,143 +1,42 @@
-// prettier-ignore
-import { Subject, BehaviorSubject, ReplaySubject, Subscriber, of, from, Observable } from 'rxjs';
-import { scan } from 'rxjs/operators';
-import { ISpec, IResource, IAction, ILocalStorage } from './interfaces';
+import { BehaviorSubject } from 'rxjs';
 
-// By default a reducer would just substitute the previous value
-function DEFAULT_REDUCER<R>(_: R, val: R): R {
-  return val;
-}
-
-function identity<R>(x: R): R { return x; }
-
-export function Resource<R>(spec: ISpec<R>, seed?: R): IResource<R> {
-  const scanner = (curr: R, { action, data }: { action: string; data: any }) =>
-    (spec[action].reducer || DEFAULT_REDUCER)(curr, data);
-  const subject = new Subject<{ action: string; data: any }>();
-  const $ = new ReplaySubject<R>(1);
-  subject.pipe(scan(scanner, seed)).subscribe($);
-  return resourceInit(spec, subject, $);
-}
-
-export function BehaviorResource<R>(spec: ISpec<R>, seed: R): IResource<R> {
-  const scanner = (curr: R, { action, data }: { action: string; data: any }) =>
-    (spec[action].reducer || DEFAULT_REDUCER)(curr, data);
-  const subject = new Subject<{ action: string; data: any }>();
-  const $ = new BehaviorSubject<R>(seed);
-  subject.pipe(scan(scanner, seed)).subscribe($);
-  return resourceInit(spec, subject, $);
-}
-
-function isPromise(p: any) {
-  return p && typeof p.then === 'function';
-}
-
-function isObservable(ob: any) {
-  return ob && typeof ob.subscribe === 'function';
-}
-
-function resourceInit<R>(
-  spec: ISpec<R>,
-  subject: Subject<{ action: string; data: any }>,
-  $: Subject<R>,
-): IResource<R> {
-  let locker = false;
-  const lock$ = new BehaviorSubject<Boolean>(locker);
-  const setLocker = (isLocked: boolean) => {
-    locker = isLocked;
-    lock$.next(locker);
-  };
-
-  type TResource<S extends ISpec<R>> = {
-    [P in keyof S]?: ((...args: any[]) => Promise<any>) | Observable<any>
-  } & {
-    $: Observable<R>;
-    lock$: Observable<Boolean>;
-  };
-
-  const result: TResource<ISpec<R>> = { $, lock$ };
-  Object.keys(spec).forEach(action => {
-    const { epic, lock } = spec[action];
-    result[action] = (...params: any): Promise<any> =>
-      new Promise((resolve, reject) => {
-        if (lock && locker) return;
-        if (lock) setLocker(true);
-        const res = epic ? epic(...params) : identity(params[0]); // first of params or undefined
-        const subscriber = Subscriber.create(
-          (data: any) => {
-            subject.next({ action, data });
-            resolve(data);
-          },
-          (e: Error) => {
-            setLocker(false);
-            reject(e);
-          },
-          () => setLocker(false),
-        );
-        if (isObservable(res)) res.subscribe(subscriber);
-        else if (isPromise(res)) from(res).subscribe(subscriber);
-        else of(res).subscribe(subscriber);
-      });
-  });
-  return result;
-}
-
-export function NaiveSetter<R>(): IAction<R, R> {
-  return { epic: (x: R) => of(x), reducer: DEFAULT_REDUCER };
-}
-
-export function NaiveResource<R>(init: R) {
-  return BehaviorResource({ set: NaiveSetter<R>() }, init);
-}
-
-function LocalStorage(): ILocalStorage {
-  const subjects: { [key: string]: any } = {};
-  const { localStorage } = window;
-  const registered = (k: string) =>
-    Object.prototype.hasOwnProperty.call(subjects, k);
-
-  return {
-    get(key: string, defaultVal: any) {
-      if (registered(key)) return subjects[key];
-      if (!localStorage.getItem(key) && defaultVal) {
-        localStorage.setItem(key, JSON.stringify(defaultVal));
-      }
-      const fromLS = localStorage.getItem(key);
-      const val = fromLS ? JSON.parse(fromLS) : defaultVal;
-      subjects[key] = new BehaviorSubject(val);
-      return subjects[key];
-    },
-    set(key: string, val: any) {
-      localStorage.setItem(key, JSON.stringify(val));
-      if (registered(key)) subjects[key].next(val);
-      return of(val);
-    },
-    del(key: string) {
-      localStorage.removeItem(key);
-      if (registered(key)) subjects[key].next(null);
-      return of(null);
-    },
-  };
-}
-
-const ls = LocalStorage();
-export function LocalStorageResource<R>(key: string, defaultVal: R) {
-  const fromLS = localStorage.getItem(key);
-  const val = fromLS ? JSON.parse(fromLS) : defaultVal;
-  window.localStorage.setItem(key, JSON.stringify(val));
-  return BehaviorResource(
-    {
-      set: { epic: v => ls.set(key, v) },
-      del: { epic: () => ls.del(key) },
-    },
-    val,
+export function Resource<R>(init: R, actions: { [s: string]: Function }) {
+  const subject = new BehaviorSubject(init);
+  return Object.assign(
+    subject,
+    Object.keys(actions).reduce((acc, k) => {
+      const action = async (...args: any[]) => {
+        const state = subject.getValue();
+        const result = await actions[k](state, ...args);
+        subject.next({ ...state, ...result });
+      };
+      return { ...acc, [k]: action };
+    }, {}),
   );
 }
 
-export function wrap(fn: (...params: any[]) => any) {
-  return (...params: any[]) => {
-    const res = fn(...params);
-    if (isPromise(res)) return from(res);
-    else return of(res);
-  };
+export const NaiveResource = <R>(init: R) =>
+  Resource(init, { set: async (_: any, val: R) => val });
+
+export function LocalStorageResource<R>(
+  key: string,
+  init: R,
+  actions: { [s: string]: Function },
+) {
+  const lsv = window.localStorage.getItem(key);
+  if (!lsv) window.localStorage.setItem(key, JSON.stringify(init));
+  const subject = new BehaviorSubject<R>(lsv ? JSON.parse(lsv) : init);
+  return Object.assign(
+    subject,
+    Object.keys(actions).reduce((acc, k) => {
+      const action = async (...args: any[]) => {
+        const state = subject.getValue();
+        const result = await actions[k](state, ...args);
+        const newVal = { ...state, ...result };
+        window.localStorage.setItem(key, JSON.stringify(newVal));
+        subject.next(newVal);
+      };
+      return { ...acc, [k]: action };
+    }, {}),
+  );
 }
