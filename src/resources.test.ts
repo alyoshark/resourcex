1868b289e16cb8b3c440c1008477ee78b6486bc7
Sayuti Daniel <sayuti@traveloka.com>
2019-09-10 23:43:48 +0800

    refactor(LocalStorageResource): use Resource
    Also included:
    - Changed test env to browser-like env assuming this library is intended for the browser
    - Fixed `NaiveResource` return `undefined`

--- src/resources.test.ts
+++ src/resources.test.ts
@@ -1,28 +1,78 @@
-import { Resource } from './resources';
+import { Resource, LocalStorageResource, NaiveResource } from './resources';
 
-const getCounter = () =>
-  Resource(
-    { val: 0 },
-    { add: ({ val }: { val: number }, amt: number) => ({ val: val + amt }) },
-  );
+const initialState = { val: 0 };
+const actions = {
+  add: ({ val }: { val: number }, amt: number) => ({ val: val + amt }),
+};
 
-test('Returning original data on initial subscription', done => {
-  getCounter().subscribe(data => {
-    expect(data.val).toBe(0);
-    done();
+describe('Resource', () => {
+  const getCounter = () => Resource(initialState, actions);
+
+  test('Returning original data on initial subscription', done => {
+    getCounter().subscribe(data => {
+      expect(data.val).toBe(0);
+      done();
+    });
+  });
+
+  test('Counter is incremented correctly', async done => {
+    const c = getCounter();
+    await c.add(1);
+    c.subscribe(data => {
+      expect(data.val).toBe(1);
+      done();
+    });
+  });
+
+  test('Throw error on overwriting Subject methods', () => {
+    const r = () => Resource({}, { getValue: () => 1 });
+    expect(r).toThrow(Error);
   });
 });
 
-test('Counter is incremented correctly', async done => {
-  const c = getCounter();
-  await c.add(1);
-  c.subscribe(data => {
-    expect(data.val).toBe(1);
-    done();
+describe('NaiveResource', () => {
+  const getCounter = () => NaiveResource(initialState);
+
+  test('Set state to given value', async () => {
+    const c = getCounter();
+    await c.set({ val: 1 });
+    c.subscribe(({ val }) => {
+      expect(val).toBe(1);
+    });
   });
 });
 
-test('Throw error on overwriting Subject methods', () => {
-  const r = () => Resource({}, { getValue: () => 1 });
-  expect(r).toThrow(Error);
+describe('LocalStorageResource', () => {
+  const getCounter = () =>
+    LocalStorageResource('counter', initialState, actions);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Set state to given value and store it in localStorage on initialization', () => {
+    getCounter();
+    expect(localStorage.getItem).toBeCalledWith('counter');
+    expect(localStorage.setItem).toBeCalledWith(
+      'counter',
+      JSON.stringify({ val: 0 }),
+    );
+  });
+
+  test('Set state to localStorage value on initialization', () => {
+    const lsv = JSON.stringify({ val: 1 });
+    (localStorage.getItem as jest.Mock).mockReturnValueOnce(lsv);
+    getCounter();
+    expect(localStorage.getItem).toBeCalledWith('counter');
+    expect(localStorage.setItem).not.toBeCalled();
+  });
+
+  test('Store new state to localStorage on action invocation', async () => {
+    const c = getCounter();
+    await c.add(1);
+    expect(localStorage.setItem).toBeCalledWith(
+      'counter',
+      JSON.stringify({ val: 1 }),
+    );
+  });
 });
